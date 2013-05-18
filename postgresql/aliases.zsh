28ea899a34952d7aa5332b08b95bfa6b1f161403
Jason Buck <jasonbuck@francisfrith.com>
2013-05-18 11:00:50 -0700

    Various key-bindings + alias updates

--- postgresql/aliases.zsh
+++ postgresql/aliases.zsh
@@ -1,2 +1,6 @@
 alias pg-up='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
 alias pg-down='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
+# From: http://zanshin.net/2013/02/02/zsh-configuration-from-the-ground-up/
+alias 'psqlstart=/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start'
+alias 'psqlstop=/usr/local/pgsql/bin/pg_ctl stop'
---
+++ tmux/aliases.zsh
@@ -0,0 +1,2 @@
+# Force tmux to use 256 colors
+alias tmux='TERM=screen-256color-bce tmux'
