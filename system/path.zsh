5de4189d13221a836f984c5033f56b7ff1155de8
Jason Buck <jasonbuck@francisfrith.com>
2014-02-18 20:31:14 -0800

    Remove brewed Python dir from path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/usr/local/share/python"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
