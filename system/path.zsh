388f30f1040e3e03c4930e85e1ac5db57e5ab6f8
Jason Buck <jasonbuck@francisfrith.com>
2014-02-19 11:34:37 -0800

    Add Postgres.app binaries to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
