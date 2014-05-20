5aeadfe0447abf058754d8aa32bfb2d44f4f00e0
Jason Buck <jasonbuck@francisfrith.com>
2014-05-20 11:38:13 -0700

    Fix prodeploy alias

--- rails/rails.zsh
+++ rails/rails.zsh
@@ -1,9 +1,9 @@
 # Rails aliases:
 alias b="bundle exec"
 alias deploy="bundle exec cap deploy"
-alias prodeploy="bundle exec cap deploy"
+alias prodeploy="bundle exec cap production deploy"
 alias log="tail -f log/development.log"
-alias stalog="bundle exec cap production log:rails"
+alias stalog="bundle exec cap staging log:rails"
 alias staglog=stalog
 alias prolog="bundle exec cap production log:rails"
 alias restart="touch tmp/restart.txt;echo '*** App restarted ***'"
