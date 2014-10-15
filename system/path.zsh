39893564681509df4529964bbdfd273f8d75f9a7
Jason Buck <jasonbuck@francisfrith.com>
2014-10-14 17:10:35 -0700

    Escape Java path correctly

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export JAVA_HOME="/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home"
